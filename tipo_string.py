e85b4b7ab0576c678f0c022465354d89a108f288
danipereira261 <dpereira261@gmail.com>
2022-07-18 20:02:37 -0300

    tipo_boleano e tipo_string

---
+++ tipo_string.py
@@ -0,0 +1,13 @@
+"""
+Tipo string
+
+Em Python, um dado é considerado do tipo string sempre que:
+-Estiver entre áspas simples -> 'uma string', '@', 'True', '42.3'
+-Estiver entre áspas duplas -> "uma string", "@", "True", "42.3"
+-Estiver entre áspas simples triplas -> '''
+-Estiver entre áspas duplas triplas -> """
+
+
+nome = 'Geek University'
+print(nome)
+print(type(nome))
