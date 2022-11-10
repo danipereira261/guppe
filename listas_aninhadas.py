fb093af8f401bd3d877015a826cf22317ca4fdcc
danipereira261 <dpereira261@gmail.com>
2022-11-10 17:22:15 -0300

    listas aninhadas

---
+++ listas_aninhadas.py
@@ -0,0 +1,40 @@
+"""
+Listas Aninhadas (Nested Lists)
+
+- Algumas linguagens de programação (C/Java) possuem uma estrutura de dados chamada de arrays:
+    - Unidimensionais (Arrays/Vetores)
+    - Multidimensionais (Matrizes)
+
+Em Python nós temos as Listas
+numeros = [1, 'b', 3.234, True, 5]
+
+
+"""
+
+# Exemplos
+listas = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]  # Matriz 3x3
+print(listas)
+print(type(listas))
+
+# Como fazemos para acessar os dados?
+print(listas[0][1])  # 2
+print(listas[2][1])  # 8
+
+for lista in listas:
+    for num in lista:
+        print(num)
+
+# List Comprehension
+[[print(valor) for valor in lista] for lista in listas]
+
+# Outros exemplos
+# Gerando um tabuleiro/matriz 3x3
+tabuleiro = [[valor for valor in range(1, 4)] for numero in range(1, 4)]
+print(tabuleiro)
+
+# Gerando jogadas para o jogo da velha
+velha = [['X' if valor % 2 == 0 else 'O' for valor in range(1, 4)] for numero in range(1, 4)]
+print(velha)
+
+# Gerando valor iniciais
+print([['*' for i in range(1, 4)] for j in range(1, 4)])
