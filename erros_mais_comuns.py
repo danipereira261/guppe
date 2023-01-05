8e9ae83df371a8414523147acbbef446f3ad5816
danipereira261 <dpereira261@gmail.com>
2023-01-05 12:15:41 -0300

    erros mais comuns

---
+++ erros_mais_comuns.py
@@ -0,0 +1,12 @@
+"""
+Erros mais comuns em Python
+
+ATENÇÃO: É importante prestar atenção e aprender a ler as saídas de erros geradas pela execução do nosso código.
+
+Erros mais comuns:
+
+SyntaxError -> Ocorre quando o Python econtra um erro de sintaxe. Ou seja, você escreveu algo que o Python
+não reconhece como parte da linguagem.
+
+"""
+
